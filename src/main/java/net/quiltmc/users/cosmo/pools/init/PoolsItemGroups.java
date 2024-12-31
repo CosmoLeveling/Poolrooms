f9b9e09da282db143220f4d75d3e94a3cc31340f
cosmo <curtisjibo@gmail.com>
2024-12-31 15:44:29 -0600

    Update PoolsItemGroups.java

--- src/main/java/net/quiltmc/users/cosmo/pools/init/PoolsItemGroups.java
+++ src/main/java/net/quiltmc/users/cosmo/pools/init/PoolsItemGroups.java
@@ -29,6 +29,7 @@ public class PoolsItemGroups {
 			itemGroup.addItem(PoolsBlocks.CUT_STEEL_WALLS);
 			itemGroup.addItem(PoolsBlocks.STEEL_GRATE);
 			itemGroup.addItem(PoolsBlocks.STEEL_BLOCK);
+			itemGroup.addItem(PoolsBlocks.CAGED_LIGHT);
 		});
 	}
 }
